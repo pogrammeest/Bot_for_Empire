69c037cc313a613dc4ab8a852b87c352b5b14f3f
2Grifon <evgeny.suraykin@gmail.com>
2020-08-14 20:02:06 +0300

    added new class sqlite

--- SQLite.py
+++ SQLite.py
@@ -1,5 +1,34 @@
+
 import sqlite3
 
+class sqlite():
+    def __init__(self,path):#метод чтения БД и создания курсора
+        self.conn=sqlite3.connect(path)
+        self.curs=self.conn.cursor()#курсор
 
-class MyClass(sqlite3):
-    pass
+    def enter(self,table_name,values):#метод ввода любых значений в любую таблицу.В аргументы ввести имя таблицы с перечислением столбцов и значения в виде кортежа
+        count=int()
+        for i in values:
+            count=count+1
+        self.curs.execute(f"INSERT INTO {table_name} VALUES ({(count-1)*'?,'} ?)",values)
+
+    def read(self,argue,table_name):#метод вывода любых значений из любой таблицы. В аргументы ввести выборку и имя таблицы
+        self.curs.execute(f"select {argue} from {table_name}")
+        rows = self.curs.fetchall()#кусок кода
+        for row in rows:#для человечекого вывода
+            print(row)#значения, а не указания ячейки памяти
+
+    def delete(self,table_name,where):#метод удаления значения с условием.В аргументы ввести название таблицы и условие
+        self.curs.execute(f"DELETE FROM {table_name} WHERE {where}")
+
+    def update(self,table_name,coloumn_name,values,where):
+        '''print(f"UPDATE {table_name} SET {coloumn_name} = '{values}' WHERE {where}")'''
+        self.curs.execute(f"UPDATE {table_name} SET {coloumn_name} = '{values}' WHERE {where}")
+
+obj=sqlite('sql.db')
+
+obj.enter('locations(name,max_lvl)',('bolota',20))#примеры использования каждого метода
+obj.read('*','locations')
+obj.update('locations','max_lvl','40','id=1')
+obj.read('*','locations')
+obj.delete('locations','id=1')
